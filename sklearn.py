2b7caefc06626592a71e9ae7fb597241188363ca
creatingashes <40171872+creatingashes@users.noreply.github.com>
2019-04-08 11:23:40 -0400

    Update and rename sklearn to sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -12,3 +12,6 @@ lm.fit(x, df.Price)
 coeff = pd.Series(lm.coef_, index = boston.feature_names)
 print('Coefficients: \n', coeff.sort_values())
 print (df.corr())
+
+#The infuence with the greatest effect on price of a house in Boston is LSTAT (% lower status of the population)
+# The least effect on the price of a house in Boston
